7fdb5709e9e42d33f35586069f4bd54634f08feb
Matt Stratton <matt.stratton@gmail.com>
2015-03-22 11:24:06 -0400

    Experiment with layout

--- web/app/themes/ado-theme/templates/header.php
+++ web/app/themes/ado-theme/templates/header.php
@@ -1,7 +1,7 @@
 <?php use Roots\Sage\Nav; ?>
 
 <header class="banner navbar navbar-default navbar-static-top" role="banner">
-  <div class="container">
+  <div class="container" id="navbar-container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
         <span class="sr-only"><?= __('Toggle navigation', 'sage'); ?></span>
@@ -9,7 +9,8 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><img src="<?= get_template_directory_uri(); ?>/dist/images/ado-logo.png" width = "259px" height "100px"alt="ADO Logo"></a>
+      <?php get_search_form(); ?>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
